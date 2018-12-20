813d081bdc7e6f9cd5ed5a471f42a151b38d6a73
Zhixuan Shao <billy1996@mail.sh.cn>
2018-12-20 08:38:05 +0800

    Add files via upload

--- test.R
+++ test.R
@@ -1,35 +1,40 @@
-require(TruncCause)
+require(mieSACE)
 ? mie
 #test
-n <- 200
+n <- 500
 set.seed(1)
 X1 <- rnorm(n)
 X2 <- rnorm(n)
 X3 <- rnorm(n)
 X4 <- rnorm(n)
 Z <- rbinom(n, 1, 0.5)
-Y <- rnorm(n,X1+X2+X3+X4+Z)
-A <- rnorm(n)
+Y <- rnorm(n, X1 + X2 + X3 + X4 + Z)
+A <- cbind(rnorm(n), rnorm(n))
 #S <- rbinom(n, 1, 0.5)
 #S <- rbinom(n,1,exp(A) / (1 + exp(A)))
-S <- rbinom(n, 1, ifelse(Z, exp(A) / (1 + exp(A)), (exp(A) / (1 + exp(A))) ^ 2))
+beta <- rep(1, ncol(A))
+S <- rbinom(n, 1, ifelse(Z, exp(A %*% beta) / (1 + exp(A %*% beta)), exp(2 * A %*% beta) / (1 + exp(2 * A %*% beta))))
+
+##naive OLS
+summary(lm(Y ~ Z + X1 + X2 + X3 + X4, subset = as.logical(S)))
 
 time = proc.time()
 t <- mie(Z, S, Y, cbind(X1, X2, X3, X4), A, optim.method = "BFGS", need.variance = TRUE)
 proc.time() - time
 
-boot.ci(t, print.progress = FALSE)$ci
-t$sace.var
-t$beta
-t$gamma
+
+#boot.result <- boot.ci(t, print.progress = FALSE)
+#boot.result$ci
+#boot.result$boot.sd
+sqrt(t$sace.var)
+#t$beta
+#t$gamma
 t$sace
 t$mu_0_LL
-t$mu_0_LL.var
+sqrt(t$mu_0_LL.var)
 t$mu_1_LL
-t$mu_1_LL.var
+sqrt(t$mu_1_LL.var)
 
 t$beta_gamma.convergence
-t$sace
 
-print(t)
-summary(t)
+selectSV(Z, S, cbind(X1, X2, X3, X4), A)
